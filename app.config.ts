9eccacbdbc2b57a502b2491078e734bb974eeb71
dmilin1 <dmilinovichiii@gmail.com>
2024-02-18 23:10:29 -0800

    eas update test

--- app.config.ts
+++ app.config.ts
@@ -1,44 +1,52 @@
 import packageJson from './package.json';
 
+const projectId = "7e403d7f-7747-4daa-a3c9-4acb948f7a60";
+
 module.exports = {
-  "expo": {
-    "name": "Hydra",
-    "slug": "hydra",
-    "version": packageJson.version,
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "hydra",
-    "userInterfaceStyle": "automatic",
-    "splash": {
-      "image": "./assets/images/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  expo: {
+    name: "Hydra",
+    slug: "hydra",
+    version: packageJson.version,
+    runtimeVersion: {
+      policy: 'appVersion',
     },
-    "assetBundlePatterns": [
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "hydra",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: [
       "**/*"
     ],
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "com.dmilin.hydra"
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.dmilin.hydra"
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
       }
     },
-    "web": {
-      "bundler": "metro",
-      "favicon": "./assets/images/favicon.png"
+    web: {
+      bundler: "metro",
+      favicon: "./assets/images/favicon.png"
     },
-    "extra": {
-      "eas": {
-        "projectId": "7e403d7f-7747-4daa-a3c9-4acb948f7a60"
+    extra: {
+      eas: {
+        projectId,
       }
     },
-    "owner": "dmilin",
-    "plugins": [
+    owner: "dmilin",
+    plugins: [
       "expo-router"
-    ]
+    ],
+    updates: {
+      url: `https://u.expo.dev/${projectId}`
+    }
   }
 }
