e4a31e3c294c600decc4f2a9c8e425dfe6d6ea3a
dmilin1 <dmilinovichiii@gmail.com>
2024-04-14 17:43:39 -0700

    basic linter setup

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    root: true,
+    extends: [
+        'universe/native',
+    ],
+    rules: {
+        // Ensures props and state inside functions are always up-to-date
+        'react-hooks/exhaustive-deps': 'warn',
+    },
+};
