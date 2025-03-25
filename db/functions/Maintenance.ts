6f80222da0d38d1e9e0309af370a5184409edeb9
ronakvir <100381130+ronakvir@users.noreply.github.com>
2025-03-24 19:37:49 -0700

    message drafts (#16)

--- db/functions/Maintenance.ts
+++ db/functions/Maintenance.ts
@@ -1,5 +1,7 @@
 import { maintainSeenPosts } from "./SeenPosts";
+import { maintainMessageDrafts } from "../../components/Modals/NewMessage";
 
 export async function doDBMaintenance() {
   await maintainSeenPosts();
+  await maintainMessageDrafts();
 }
