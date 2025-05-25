671092dc0c129429a4f563325b9eb07a461501af
dmilin1 <dmilinovichiii@gmail.com>
2025-05-25 13:07:33 -0700

    fix bug with long text in markdown editor being hidden

--- components/UI/MarkdownEditor.tsx
+++ components/UI/MarkdownEditor.tsx
@@ -108,7 +108,6 @@ export default function MarkdownEditor({
         placeholder={placeholder}
         placeholderTextColor={theme.verySubtleText}
         multiline
-        numberOfLines={4}
         value={text}
         onChangeText={setText}
         onSelectionChange={({ nativeEvent }) => {
