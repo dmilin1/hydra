93ef68fe2c5c756324b2f6aaf43f58195a47339a
dmilin1 <dmilinovichiii@gmail.com>
2025-08-06 21:31:41 -0700

    fix videos still playing after exiting full screen when autoplay is turned off

--- components/RedditDataRepresentations/Post/PostParts/PostMediaParts/VideoPlayer.tsx
+++ components/RedditDataRepresentations/Post/PostParts/PostMediaParts/VideoPlayer.tsx
@@ -206,6 +206,8 @@ export default function VideoPlayer({
                     exitedFullScreenCallback?.();
                     if (autoPlayVideos) {
                       player.play();
+                    } else {
+                      player.pause();
                     }
                   }}
                   onPictureInPictureStop={() => {
